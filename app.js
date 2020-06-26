f8da89022d81fc75e14c54baa6d2e2352b732975
sarath <https://github.com/sarathrj10>
2020-06-26 11:29:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 //init storage class
 const storage = new Storageweather();
-//get stored location data
+//removining stored data to access current location
+storage.removeitem();
+//set current location to default
+storage.setCurrentLocation().then(() =>{
+ //get stored location data
 const storedLocation = storage.getLocationData();
 //init weather class
 const weather = new Weather(storedLocation.city,storedLocation.state);
@@ -8,37 +12,43 @@ const weather = new Weather(storedLocation.city,storedLocation.state);
 const ui = new UI();
 
 //add eventlistner when DOM Loads
-document.addEventListener('DOMContentLoaded', getWeather);
-
+firstAttempt();
 //Change location Event
 document.getElementById('w-changebtn').addEventListener('click',() => {
     const city =document.getElementById('city').value;
     const state =document.getElementById('state').value;
 
     weather.changeLocation(city,state);
-
-    //change loaction in ls
-
-    storage.setLocationData(city,state);
-
     getWeather();
 
     //hide model
     $('#locModal').modal('hide');
 })
-
+function firstAttempt(){
+    weather.getWeather()
+.then((results) => {
+   ui.paint(results);
+})
+.catch((err) => {
+    // console.log('not found ...moving to district')
+       storage.setDistrict().then(() =>{
+        const newLocation = storage.getLocationData();
+        weather.changeLocation(newLocation.city,newLocation.state)
+       getWeather();
+   })
+})
+}
 function getWeather(){
 weather.getWeather()
 .then((results) => {
-    console.log(results)
    ui.paint(results);
 })
 .catch((err) => {
     ui.showAlert();
-    storage.removeitem(city);
-    storage.removeitem(state);
+    storage.removeitem();
     setTimeout(() => {
         location.reload(false);
         },2000)
 })
 }
+})
